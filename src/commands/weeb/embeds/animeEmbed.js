82eaeb5d6549474898ccfd9713b64bd8713f9a14
BlueOrange <this.is.blueorange@gmail.com>
2020-05-01 18:02:27 +0545

    embed cleanedup

--- src/commands/weeb/embeds/animeEmbed.js
+++ src/commands/weeb/embeds/animeEmbed.js
@@ -1,5 +1,6 @@
 const { MessageEmbed } = require('discord.js');
 
+const feilds = ['status', 'duration', 'score', 'broadcast', 'rank', 'episodes'];
 const animeEmbed = async (animeInfo) => {
   const embedMsg = new MessageEmbed();
   embedMsg
@@ -7,6 +8,18 @@ const animeEmbed = async (animeInfo) => {
     .setURL(animeInfo.url)
     .setAuthor(animeInfo.rating)
     .setDescription(animeInfo.synopsis)
+
+    .setImage(animeInfo.image_url)
+    .setFooter(
+      'API provided by Jikan',
+      'https://jikan.moe/assets/images/logo/jikan.logo.png'
+    );
+
+  Object.keys(animeInfo).forEach((key) => {
+    console.log(key, animeInfo.key);
+    feilds.includes(key) ? embedMsg.addField(key, animeInfo[key], true) : null;
+  });
+  /*
     .addFields(
       { name: 'Status', value: animeInfo.status, inline: true },
       { name: 'Duration', value: animeInfo.duration, inline: true },
@@ -14,13 +27,8 @@ const animeEmbed = async (animeInfo) => {
       { name: 'Broadcast', value: animeInfo.broadcast, inline: true },
       { name: 'Rank', value: animeInfo.rank, inline: true },
       { name: 'Episodes', value: animeInfo.episodes, inline: true }
-    )
-    .setImage(animeInfo.image_url)
-    .setFooter(
-      'API provided by Jikan',
-      'https://jikan.moe/assets/images/logo/jikan.logo.png'
     );
-
+    */
   return embedMsg;
 };
 
--- src/commands/weeb/embeds/mangaEmbed.js
+++ src/commands/weeb/embeds/mangaEmbed.js
@@ -1,5 +1,6 @@
 const { MessageEmbed } = require('discord.js');
 
+const feilds = ['status', 'volumes', 'score', 'chapters', 'rank'];
 const mangaEmbed = async (mangaInfo) => {
   const embedMsg = new MessageEmbed();
   embedMsg
@@ -7,20 +8,27 @@ const mangaEmbed = async (mangaInfo) => {
     .setURL(mangaInfo.url)
     .setAuthor(mangaInfo.authors[0].name)
     .setDescription(mangaInfo.synopsis)
-    .addFields(
+    .setImage(mangaInfo.image_url)
+    .setFooter(
+      'API provided by Jikan',
+      'https://jikan.moe/assets/images/logo/jikan.logo.png'
+    )
+    .addField('Published', mangaInfo.published.string, true);
+
+  Object.keys(mangaInfo).forEach((key) => {
+    console.log(key, mangaInfo.key);
+    feilds.includes(key) ? embedMsg.addField(key, mangaInfo[key], true) : null;
+  });
+  /*
+    .addField(
       { name: 'Status', value: mangaInfo.status, inline: true },
       { name: 'Volumes', value: mangaInfo.volumes, inline: true },
       { name: 'Score', value: mangaInfo.score, inline: true },
       { name: 'Chapters', value: mangaInfo.chapters, inline: true },
       { name: 'Rank', value: mangaInfo.rank, inline: true },
       { name: 'Published', value: mangaInfo.published.string, inline: true }
-    )
-    .setImage(mangaInfo.image_url)
-    .setFooter(
-      'API provided by Jikan',
-      'https://jikan.moe/assets/images/logo/jikan.logo.png'
     );
-
+*/
   return embedMsg;
 };
 
